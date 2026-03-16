23d62eccda98e9c11de22c9400c65e0c6bce281f
bhargav-devops-7 <bhargavdodda996@gmail.com>
2026-03-16 13:58:26 +0530

    terraform-ec2-module

---
+++ variables.tf
@@ -0,0 +1,23 @@
+variable ami_id {
+  type        = string
+  description = "this is the AMI ID used for instance creation"
+}
+
+variable instance_type{
+  type = string
+  description = "this is the instance type used for ec2 creation"
+
+  validation {
+    condition = contains([t3.micro, t3.small, t3.medium], instance_type)
+    error_message = "please select t3.micro or t3.small or t3.medium"
+  }
+}
+
+variable sg_ids {
+  type = list
+}
+
+variable ec2_tags{
+  type = map
+  default = {}
+}
